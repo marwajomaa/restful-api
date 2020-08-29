dd3d0e3e6ab1be6998f08afe2b6aed9c22c1b22e
marwa <marwajomaa4@gmail.com>
2020-08-29 21:36:06 +0300

    get tours stats using aggregation

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -7,13 +7,16 @@ const {
   deleteTour,
   updateTour,
   aliasTopTours,
+  getTourStats,
 } = require('./../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
 
